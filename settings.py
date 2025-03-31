0acd7b5af099ca1081ed80d0468bf3ab98957216
Tom Bewley <tom.bewley@jpmorgan.com>
2025-03-31 20:55:56 +0100

    Add template settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+"""
+This file contains all the settings and secrets for the project.
+DO NOT COMMIT TO THE REPOSITORY AFTER THESE HAVE BEEN ADDED.
+"""
+
+# Project ID of Descope project (https://app.descope.com/settings/project)
+DESCOPE_PROJECT_ID = "ADD YOUR PROJECT ID HERE"
+# ID of desired Descope flow
+DESCOPE_FLOW = "sign-up-or-in"
+# Port to serve web app on
+SERVER_PORT = 8080
+# Private key used to secure server-side storage
+STORAGE_SECRET = "ADD YOUR SECRET HERE"
